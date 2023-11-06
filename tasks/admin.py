fe565f5ff853aea33f9372fd2122e6ce9264a95a
Ajay Ghosh <ajayghosh28@gmail.com>
2023-11-06 19:46:06 +0600

    changed admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -5,9 +5,31 @@ from .models import *
 # Register your models here.
 # User = get_user_model()
 
+# Admin class for Photo model
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'image']
+    list_display = ('image',)
 
-admin.site.register(User)
+# Admin class for Task model
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'due_date', 'priority', 'is_complete', 'created_at', 'updated_at', 'user')
+    list_filter = ('priority', 'is_complete')
+    search_fields = ('title', 'description', 'user__username')
+    list_editable = ('is_complete',)
+    list_per_page = 20
+    list_max_show_all = 100
+    date_hierarchy = 'due_date'
+    actions = ['mark_as_complete', 'mark_as_incomplete']
+
+    def mark_as_complete(self, request, queryset):
+        queryset.update(is_complete=True)
+    mark_as_complete.short_description = "Mark selected tasks as complete"
+
+    def mark_as_incomplete(self, request, queryset):
+        queryset.update(is_complete=False)
+    mark_as_incomplete.short_description = "Mark selected tasks as incomplete"
+
+    ordering = ('priority',)  # Sort by priority by default
+
+# Register the admin classes
 admin.site.register(Photo, PhotoAdmin)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
